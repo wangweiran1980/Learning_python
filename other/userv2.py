cb4f75b51a3fc363cf4abc8f3d1364e6cd306aa0
wangweiran <wang_weiran@163.com>
2022-01-07 16:25:40 +0800

    userv2 final

--- other/userv2.py
+++ other/userv2.py
@@ -4,67 +4,100 @@ Created on Mon Jan  3 15:17:43 2022.
 @author: wangweiran
 """
 
-def add(txt-> str):
+users = {}
+
+
+def isInclude(data: list, txt: str, precise: bool = True) -> bool:
+    """在列表中查询是否包含给定的文本."""
+    if precise:
+        if txt in data:
+            return True
+    else:
+        for item in data:
+            if txt in item:
+                return True
+    return False
+
+
+def getID() -> int:
+    """在添加用户信息时指定ID."""
+    if users == {}:
+        return 1
+    return list(users.keys())[-1] + 1
+
+
+def add():
+    """添加用户信息."""
+    txt = input("请输入用户信息(name, age, tel): ")
     nodes = txt.split(",")
-    if len(users) != 0:
-        for id in users:
-            if nodes[0].strip() != users[id][0]:
     if len(nodes) != 3:
         print("输入信息错误")
-        continue
-    id = len(users) + 1
-    users[id] = [
-        nodes[0].strip(),
-        nodes[1].strip(),
-        nodes[2].strip(),
-        ]
+    for user in users.values():
+        if isInclude(user, nodes[0].strip()):
+            print('名字已存在!')
+            return
+    id = getID()
+    users[id] = [nodes[0].strip(), nodes[1].strip(), nodes[2].strip()]
 
-users = {}
+
+def delete():
+    """删除用户信息."""
+    name = input("请输入名字: ")
+    for id in users:
+        if isInclude(users[id], name):
+            users.pop(id)
+            return
+    print("名字不存在!")
+
+
+def show():
+    """展示用户数据."""
+    for user in users:
+        print("{id} {name} {age} {tel}".format(
+            id=user,
+            name=users[user][0],
+            age=users[user][1],
+            tel=users[user][2],
+        ))
+
+
+def edit():
+    """编辑已存在用户信息."""
+    id = int(input("请输入ID: "))
+    if id not in users:
+        print('ID不存在!')
+        return
+    txt = input("请输入用户信息(name, age, tel): ")
+    nodes = txt.split(",")
+    if len(nodes) != 3:
+        print("输入信息错误")
+        return
+    users[id] = [nodes[0].strip(), nodes[1].strip(), nodes[2].strip()]
+
+
+def find():
+    """查找用户信息."""
+    txt = input("请输入要查询的内容: ").strip()
+    for user in users.values():
+        if isInclude(user, txt, False):
+            print("{name} {age} {tel}".format(name=user[0],
+                                              age=user[1],
+                                              tel=user[2]))
+            return
+    print('未找到符合数据')
+
 
 while True:
     op = input("请选择操作(add, del, edit, list, find, quit): ").strip()
     if op == "quit":
         break
     elif op == "add":
-        txt = input("请输入用户信息(name, age, tel): ")
-        
-        # print(users)
+        add()
     elif op == "del":
-        name = input("请输入名字: ")
-        for id in users:
-            if name in users[id]:
-                users.pop(id)
-                break
-        else:
-            print("名字不存在!")
+        delete()
     elif op == "list":
-        for user in users:
-            print(
-                "{id} {name} {age} {tel}".format(
-                    id=user,
-                    name=users[user][0],
-                    age=users[user][1],
-                    tel=users[user][2],
-                )
-            )
+        show()
     elif op == "edit":
-        id = int(input("请输入ID: "))
-        if id in users:
-            txt = input("请输入用户信息(name, age, tel): ")
-            nodes = txt.split(",")
-            if len(nodes) != 3:
-                print("输入信息错误")
-                continue
-            users[id] = [nodes[0].strip(), nodes[1].strip(), nodes[2].strip()]
-            continue
-        print("ID不存在!")
+        edit()
     elif op == "find":
-        txt = input("请输入要查询的内容: ")
-        for id in users:
-            for user in users[id]:
-                if txt in user:
-                    print(
-                        "{id} {name} {age} {tel}".format(
-                            id=id, name=users[id][0], age=users[id][1], tel=users[id][2]
-                        )
-                    )
+        find()
