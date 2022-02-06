3e34863428edb955b655a1bee32719026ba9ec93
wangweiran <wang_weiran@163.com>
2022-02-07 07:34:21 +0800

    day06

--- cmdb/user/models.py
+++ cmdb/user/models.py
@@ -1,3 +1,15 @@
+import json
 from django.db import models
 
 # Create your models here.
+
+DATA_FILE = './data.json'
+
+
+def get_users() -> list[dict, str]:
+    try:
+        with open(DATA_FILE, 'r') as f:
+            users = json.load(f)
+    except FileNotFoundError as err:
+        return {}, err
+    return users, ''
--- cmdb/user/views.py
+++ cmdb/user/views.py
@@ -1,29 +1,11 @@
 from django.shortcuts import render
-import time
-# from django.http import HttpResponse
+from .models import get_users
 
 # Create your views here.
 
-users = {
-    '1': {
-        'name': 'Jack',
-        'gender': 1,
-        'age': 20,
-        'tel': '987'
-    },
-    '2': {
-        'name': 'Rose',
-        'gender': 0,
-        'age': 16,
-        'tel': '765'
-    },
-}
-
 
 def index(request):
-    # return HttpResponse('我的第一个网页')
-    # return render(request, 'user/index.html')
-    return render(request, 'user/index.html', {
-        'current_time': time.time(),
-        'users': users.items()
-    })
+    users = get_users()
+    if users[0]:
+        return render(request, 'user/index.html', {'users': users[0].items()})
+    return render(request, 'user/404.html', {'error': users[-1]})
